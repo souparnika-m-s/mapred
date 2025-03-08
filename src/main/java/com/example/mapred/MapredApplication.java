e0580942442f433eec8fe5bda309f644c0661041
souparnikams <souparnika.ms@flytxt.com>
2025-03-08 16:37:12 +0530

    Initial commit

---
+++ src/main/java/com/example/mapred/MapredApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapredApplication.class, args);
+	}
+
+}
